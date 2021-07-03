61d1d67966a80618544435c210ccd8abbde1aac4
Nate Choe <thetinybros@gmail.com>
2021-07-03 09:26:40 -0500

    Licensed!

--- src/fbgl.h
+++ src/fbgl.h
@@ -1,3 +1,23 @@
+/*
+ * fbgl: A linux framebuffer graphics library
+ * Copyright (C) 2021  Nate Choe
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Email me: natechoe9@gmail.com
+ * */
+
 typedef struct {
 	int *fb;
 	//mmaped to /dev/fbX. Each value is of the form 0x00rrggbb with the
